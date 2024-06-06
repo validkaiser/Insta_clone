98c867f346fdc1ab0392f7cd7e07291747792f15
validkaiser <94759357+validkaiser@users.noreply.github.com>
2024-06-06 23:31:00 +0300

    update

---
+++ vite-project/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,13 @@
+import {Container, Flex} from "@chakra-ui/react";
+
+const AuthPage = () => {
+  return (
+    <Flex minH={"100vh"} justifyContent ={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+      blah bla blah
+      </Container>
+    </Flex>
+  );
+};
+
+export default AuthPage;
